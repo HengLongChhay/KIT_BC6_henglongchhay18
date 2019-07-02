05bb0633a63f81d45ed649e9e6bd98dc8fe96600
HengLongChhay <45729994+HengLongChhay@users.noreply.github.com>
2019-07-02 14:37:07 +0700

    Add files via upload

---
+++ henglongchhay18/week1/ex/e03_hello.py
@@ -0,0 +1,10 @@
+print("Enter a number:")
+num=input()
+
+if num=="":
+    print("Nothing to display")
+else:
+    for i in range(0,int(num)):
+        print("Hello World!")
+
+
---
+++ henglongchhay18/week1/ex/e04_max.py
@@ -0,0 +1,14 @@
+print("Enter a number:")
+num1=input()
+print("Enter second number:")
+num2=input()
+if num1>num2:
+    print("Result: "+num1+" > "+num2)
+else if num2>num1:
+    print()
+
+else:
+    print("NUMBER_01 == NUMBER_02")
+
+
+
